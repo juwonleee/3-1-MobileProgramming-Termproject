6aae463557dd9ec040536119c0b0e0b3d212797a
Chosunhyun <hypo1016@gmail.com>
2024-05-29 21:43:36 +0900

    add exercise description function

--- app/src/main/java/com/example/nako/ExerciseFragment.java
+++ app/src/main/java/com/example/nako/ExerciseFragment.java
@@ -5,6 +5,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 
 public class ExerciseFragment extends Fragment {
@@ -12,6 +15,14 @@ public class ExerciseFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_exercise, container, false);
+        View view = inflater.inflate(R.layout.fragment_exercise, container, false);
+
+        ViewCompat.setOnApplyWindowInsetsListener(view.findViewById(R.id.exerciseFragment), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        return view;
     }
 }
